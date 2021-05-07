ff980fad94d1d2425d49c11cce2c97990f61bbfd
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-05-06 22:43:33 -0400

    Created Home, personal, profile, interest, & summary page

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -15,6 +18,40 @@ $f3->route('GET /', function (){
     // Display the home page
     $view = new Template();
     echo $view -> render('views/home.html');
+});
+
+$f3->route('GET|POST /personal', function (){
+
+    /* If the form has been submitted, add the data to session
+     * and send the user to the next order form
+    */
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        var_dump($_POST);
+
+        header('location: profile');
+    }
+
+    // Display the Personal info page
+    $view = new Template();
+    echo $view -> render('views/personal_Info.html');
+});
+
+$f3->route('GET /profile', function (){
+    // Display the Profile page
+    $view = new Template();
+    echo $view -> render('views/profile.html');
+});
+
+$f3->route('GET /interest', function (){
+    // Display the Interest page
+    $view = new Template();
+    echo $view -> render('views/interest.html');
+});
+
+$f3->route('GET /summary', function (){
+    // Display the summary page
+    $view = new Template();
+    echo $view -> render('views/summary.html');
 });
 
 //Run fat free
