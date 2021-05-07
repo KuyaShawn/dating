ed56bd235a6a93b7dbdea5ba47569d43bc1f3c2c
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-05-07 00:09:50 -0400

    updated index.php

--- index.php
+++ index.php
@@ -14,44 +14,73 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define a default route
-$f3->route('GET /', function (){
+$f3->route('GET /', function () {
     // Display the home page
     $view = new Template();
-    echo $view -> render('views/home.html');
+    echo $view->render('views/home.html');
 });
 
-$f3->route('GET|POST /personal', function (){
+$f3->route('GET|POST /personal', function () {
 
     /* If the form has been submitted, add the data to session
      * and send the user to the next order form
     */
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        var_dump($_POST);
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        $_SESSION['fName'] = $_POST['fName'];
+        $_SESSION['lName'] = $_POST['lName'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['gender'] = $_POST['gender'];
+        $_SESSION['pNum'] = $_POST['pNum'];
         header('location: profile');
     }
 
     // Display the Personal info page
     $view = new Template();
-    echo $view -> render('views/personal_Info.html');
+    echo $view->render('views/personal_Info.html');
 });
 
-$f3->route('GET /profile', function (){
+$f3->route('GET|POST /profile', function () {
+    /* If the form has been submitted, add the data to session
+    * and send the user to the next order form
+    */
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['state'] = $_POST['state'];
+        $_SESSION['seeking'] = $_POST['seeking'];
+        $_SESSION['bio'] = $_POST['bio'];
+
+        header('location: interest');
+    }
+
     // Display the Profile page
     $view = new Template();
-    echo $view -> render('views/profile.html');
+    echo $view->render('views/profile.html');
 });
 
-$f3->route('GET /interest', function (){
+$f3->route('GET|POST /interest', function () {
+
+    /* If the form has been submitted, add the data to session
+    * and send the user to the next order form
+    */
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+        $_SESSION['in_door'] = implode(" ", $_POST['in_door']);
+        $_SESSION['out_door'] = implode(' ', $_POST['out_door']);
+
+        header('location: summary');
+    }
+
     // Display the Interest page
     $view = new Template();
-    echo $view -> render('views/interest.html');
+    echo $view->render('views/Interest.html');
 });
 
-$f3->route('GET /summary', function (){
+$f3->route('GET /summary', function () {
     // Display the summary page
     $view = new Template();
-    echo $view -> render('views/summary.html');
+    echo $view->render('views/summary.html');
 });
 
 //Run fat free
