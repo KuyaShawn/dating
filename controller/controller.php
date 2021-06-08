b12086f5a4c44fbf579a7918241d00e3261da92f
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-06-08 00:01:32 -0400

    fixed a problem & made my mode cleaner

--- controller/controller.php
+++ controller/controller.php
@@ -29,51 +29,43 @@ class Controller
             //var_dump($_POST);
             $_SESSION = array();
 
-            //Initialize variables to store user input
-            $userFirst = "";
-            $userLast = "";
-            $userAge = "";
-            $userPhone = "";
-            $userGender = "";
-            $userPremium = false;
-
-            $userFirst = $_POST['fName'];
-            $userLast = $_POST['lName'];
-            $userAge = $_POST['age'];
-            $userPhone = $_POST['pNum'];
-            $userGender = $_POST['gender'];
+            // When user clicks on the premium checkbox
             $userPremium = isset($_POST['premium']);
-
             if ($userPremium) {
                 $_SESSION['user'] = new PremiumMember();
             } else {
                 $_SESSION['user'] = new Member();
             }
 
+            $userFirst = $_POST['fName'];
             if (Validation::validName($userFirst)) {
                 $_SESSION['user']->setFname($userFirst);
             } else {
                 $this->_f3->set('errors["fname"]', "Please input a valid first name");
             }
 
+            $userLast = $_POST['lName'];
             if (Validation::validName($userLast)) {
                 $_SESSION['user']->setLname($userLast);
             } else {
                 $this->_f3->set('errors["lname"]', "Please input a valid last name");
             }
 
+            $userAge = $_POST['age'];
             if (Validation::validAge($userAge)) {
                 $_SESSION['user']->setAge($userAge);
             } else {
                 $this->_f3->set('errors["age"]', "Please enter a valid age");
             }
 
+            $userPhone = $_POST['pNum'];
             if (Validation::validPhone($userPhone)) {
                 $_SESSION['user']->setPhone($userPhone);
             } else {
                 $this->_f3->set('errors["pNum"]', "Please enter a valid phone number");
             }
 
+            $userGender = $_POST['gender'];
             if (!is_null($userGender)) {
                 $_SESSION['user']->setGender($userGender);
             } else {
@@ -103,32 +95,24 @@ class Controller
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             //var_dump($_POST);
 
-            //Initialize variables to store user input
-            $userEmail = "";
-            $userState = "";
-            $userBio = "";
-            $userSeeking = "";
-
-
             $userEmail = $_POST['email'];
-            $userState = $_POST['state'];
-            $userBio = $_POST['bio'];
-            $userSeeking = $_POST['seeking'];
-
             if (Validation::validEmail($userEmail)) {
                 $_SESSION['user']->setEmail($userEmail);
             } else {
                 $this->_f3->set('errors["email"]', "Please enter a valid email address");
             }
 
+            $userSeeking = $_POST['seeking'];
             if (!is_null($userSeeking)) {
                 $_SESSION['user']->setSeeking($userSeeking);
             } else {
                 $this->_f3->set('errors["seeking"]', "Please choose the gender you're interested in");
             }
 
+            $userState = $_POST['state'];
             $_SESSION['user']->setState($userState);
 
+            $userBio = $_POST['bio'];
             $_SESSION['user']->setBio($userBio);
 
             //Store the user input in the hive (Part of making the code sticky)
@@ -154,9 +138,6 @@ class Controller
             $userIndoor = array();
             $userOutdoor = array();
 
-            /* If the form has been submitted, add the data to session
-            * and send the user to the next order form
-            */
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $userIndoor = $_POST['indoorInterests'] == null ? array() : $_POST['indoorInterests'];
                 $userOutdoor = $_POST['outdoorInterests'] == null ? array() : $_POST['outdoorInterests'];
